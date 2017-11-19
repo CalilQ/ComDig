36cfa28ee2d53d8e87f47cc4a0309f8b37990fe8
CalilQ <calil_queiroz@hotmail.com>
2017-11-19 15:51:08 -0200

    Make Hello World

---
+++ 2. How to install/hello_world.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 19 15:17:58 2017
+
+@author: Calil
+"""
+
+# Comentarios com o sinal de hash (#)
+
+'''
+Ou como um bloco, usando tres aspas simples.
+'''
+
+"""
+Aspas duplas tambem funcionam.
+"""
+
+# Imprimir na tela
+print("Hello World")
+
+# Solicitar dados do usuario
+print("Qual o seu nome?")
+nome = input();
+
+# Imprimir novamente
+print("Hello " + nome)
---
+++ 3. Data types and operators/arithmetic_operators.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 19 15:49:02 2017
+
+@author: Calil
+"""
+
